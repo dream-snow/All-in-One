59e6a11204e9d63a7f1105395c5899e16928d627
dream-snow <57491031+dream-snow@users.noreply.github.com>
2021-05-03 08:44:08 +0800

    Update diy-1.sh

--- build/Lede_x86_64/diy-1.sh
+++ build/Lede_x86_64/diy-1.sh
@@ -14,7 +14,7 @@ git clone https://github.com/jerrykuku/luci-app-vssr package/luci-app-vssr
 git clone https://github.com/vernesong/OpenClash package/luci-app-openclash
 git clone https://github.com/godros/luci-app-godproxy.git package/luci-app-godproxy
 
-git clone https://github.com/garypang13/luci-app-bypass package/luci-app-bypass
+# git clone https://github.com/garypang13/luci-app-bypass package/luci-app-bypass
 svn co https://github.com/garypang13/openwrt-packages/trunk/lua-maxminddb
-find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}
-find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
+# find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}
+# find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
