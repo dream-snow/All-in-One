9ff5291b9ba2f70f180acbfd2e86b56d775e0696
dream-snow <57491031+dream-snow@users.noreply.github.com>
2021-08-12 18:31:57 +0800

    Update diy-part.sh

--- build/Lede_source/diy-part.sh
+++ build/Lede_source/diy-part.sh
@@ -8,18 +8,33 @@
 
 
 
-# cat >$NETIP <<-EOF
-# uci set network.lan.ipaddr='192.168.2.2'                                    # IPv4 地址(openwrt后台地址)
-# uci set network.lan.netmask='255.255.255.0'                                 # IPv4 子网掩码
-# uci set network.lan.gateway='192.168.2.1'                                   # IPv4 网关
-# uci set network.lan.broadcast='192.168.2.255'                               # IPv4 广播
-# uci set network.lan.dns='114.114.114.114 223.5.5.5'                         # DNS(多个DNS要用空格分开)
-# uci set network.lan.delegate='0'                                            # 去掉LAN口使用内置的 IPv6 管理
-# uci commit network                                                          # 不要删除跟注释,除非上面全部删除或注释掉了
-#uci set dhcp.lan.ignore='1'                                                 # 关闭DHCP功能
-#uci commit dhcp                                                             # 跟‘关闭DHCP功能’联动,同时启用或者删除跟注释
+  cat >$NETIP <<-EOF
+  uci set network.lan.type='bridge'
+  uci set network.lan.ifname='eth1 eth2 eth3'
+  uci set network.lan.proto='static'
+  uci set network.lan.ipaddr='192.168.20.2'                                    # IPv4 地址(openwrt后台地址)
+  uci set network.lan.netmask='255.255.255.0'                                 # IPv4 子网掩码
+  uci set network.lan.gateway='192.168.20.1'                                   # IPv4 网关
+  uci set network.lan.broadcast='192.168.20.255'                               # IPv4 广播
+  uci set network.lan.dns='114.114.114.114 223.5.5.5'                         # DNS(多个DNS要用空格分开)
+  uci set network.lan.delegate='0'                                            # 去掉LAN口使用内置的 IPv6 管理
+  
+  uci set network.wan.ifname='eth0'
+  uci set network.wan._orig_ifname='eth0'
+  uci set network.wan._orig_bridge='false'
+  uci set network.wan.proto='pppoe'
+  uci set network.wan.password='123456'
+  uci set network.wan.ipv6='auto'
+  uci set network.wan.keepalive='0'
+  #uci set network.wan.username=''
+  uci set network.wan.delegate='0'
+  
+  uci commit network                                                          # 不要删除跟注释,除非上面全部删除或注释掉了
+  uci set dhcp.lan.ignore='1'                                                 # 关闭DHCP功能
+  uci commit dhcp                                                             # 跟‘关闭DHCP功能’联动,同时启用或者删除跟注释
 # uci set system.@system[0].hostname='OpenWrt-123'                            # 修改主机名称为OpenWrt-123
-# EOF
+  EOF
+
 
 
 # sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile           # 选择argon为默认主题
